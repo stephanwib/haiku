e5d133c40d5e370e620e00d2b4deceb4e60a971f
stephanwib <stephanwib@googlemail.com>
2025-07-03 13:25:13 +0200

    Transplant VolumeRoster.cpp

--- src/kits/storage/VolumeRoster.cpp
+++ src/kits/storage/VolumeRoster.cpp
@@ -14,12 +14,14 @@
 #include <Bitmap.h>
 #include <Directory.h>
 #include <fs_info.h>
+#include <kernel_interface.h>
 #include <Node.h>
 #include <NodeMonitor.h>
 #include <VolumeRoster.h>
+#include <List.h>
 
 
-static const char kBootVolumePath[] = "/boot";
+static const char kBootVolumePath[] = "/";
 
 using namespace std;
 
@@ -33,6 +35,28 @@ BVolumeRoster::BVolumeRoster()
 	: fCookie(0),
 	  fTarget(NULL)
 {
+	struct mntent*	aMountEntry;
+	FILE*			fstab;
+
+	fstab = setmntent (_PATH_MOUNTED, "r");
+
+	while ((aMountEntry = getmntent(fstab)))
+	{
+		// For now, we only keep items mounted at / or /media
+		if (aMountEntry->mnt_dir != NULL &&
+			(strcmp(aMountEntry->mnt_dir, "/") == 0	|| strncmp(aMountEntry->mnt_dir, "/media", 6) == 0)) {
+			mMountList.AddItem(new BVolume(aMountEntry));
+		}
+	}
+
+	if (endmntent(fstab) == 0)
+	{
+		int saved_errno = errno;
+
+		_DeallocateMountList();
+
+		errno = saved_errno;
+	}
 }
 
 
@@ -40,6 +64,7 @@ BVolumeRoster::BVolumeRoster()
 BVolumeRoster::~BVolumeRoster()
 {
 	StopWatching();
+	_DeallocateMountList();
 }
 
 
@@ -50,15 +75,22 @@ BVolumeRoster::GetNextVolume(BVolume *volume)
 	// check parameter
 	status_t error = (volume ? B_OK : B_BAD_VALUE);
 	// get next device
-	dev_t device;
-	if (error == B_OK) {
-		device = next_dev(&fCookie);
-		if (device < 0)
-			error = device;
+	BVolume*	aMount;
+
+	if (!error)
+	{
+		aMount = (BVolume*)(mMountList.ItemAt(fCookie++));
+
+		if (aMount != NULL)
+		{
+			*volume = *aMount;
+		}
+		else
+		{
+			error = B_ERROR;
+		}
 	}
-	// init volume
-	if (error == B_OK)
-		error = volume->SetTo(device);
+
 	return error;
 }
 
@@ -76,20 +108,31 @@ status_t
 BVolumeRoster::GetBootVolume(BVolume *volume)
 {
 	// check parameter
-	status_t error = (volume ? B_OK : B_BAD_VALUE);
+	if (!volume)
+		return B_BAD_VALUE;
+
 	// get device
-	dev_t device;
-	if (error == B_OK) {
-		device = dev_for_path(kBootVolumePath);
-		if (device < 0)
-			error = device;
+	for (int32 i = 0; i < mMountList.CountItems(); i++) {
+		BVolume* aMount = static_cast<BVolume*>(mMountList.ItemAt(i));
+		if (aMount && aMount->mMountPath == kBootVolumePath) {
+			*volume = *aMount;
+			return B_OK;
+		}
 	}
-	// init volume
-	if (error == B_OK)
-		error = volume->SetTo(device);
-	return error;
+
+	return B_ENTRY_NOT_FOUND;
 }
 
+
+void	BVolumeRoster::_DeallocateMountList()
+{
+	BVolume *aMount;
+
+	while ((aMount = (BVolume*)(mMountList.RemoveItem(1L))) != NULL)
+	{
+		delete aMount;
+	}
+}
 
 // Starts watching the available volumes for changes.
 status_t
