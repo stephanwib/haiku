3967abbf005895ef1f126725dd6ce8b3fecb2614
stephanwib <stephanwib@googlemail.com>
2026-01-12 08:39:54 +0100

    Transplant from Cosmoe

--- src/servers/app/AppServer.cpp
+++ src/servers/app/AppServer.cpp
@@ -12,10 +12,7 @@
 
 #include "AppServer.h"
 
-#include <syslog.h>
-
 #include <AutoDeleter.h>
-#include <LaunchRoster.h>
 #include <PortLink.h>
 
 #include "BitmapManager.h"
@@ -49,11 +46,16 @@ uint32 gAppServerSIMDFlags = 0;
 */
 AppServer::AppServer(status_t* status)
 	:
-	SERVER_BASE("application/x-vnd.Haiku-app_server", "picasso", -1, false,
-		status),
+	MessageLooper("app_server"),
+	fMessagePort(-1),
 	fDesktopLock("AppServerDesktopLock")
 {
-	openlog("app_server", 0, LOG_DAEMON);
+	fMessagePort = create_port(DEFAULT_MONITOR_PORT_SIZE, SERVER_PORT_NAME);
+	if (fMessagePort < B_OK)
+		debugger("app_server could not create message port");
+
+	fLink.SetReceiverPort(fMessagePort);
+
 
 	gInputManager = new InputManager();
 
@@ -80,6 +82,8 @@ AppServer::AppServer(status_t* status)
 	BLaunchRoster().Target("login", data);
 #endif
 #endif
+	if (status != NULL)
+		*status = B_OK;
 }
 
 
@@ -95,27 +99,49 @@ AppServer::~AppServer()
 
 	gFontManager->Lock();
 	gFontManager->Quit();
-
-	closelog();
 }
 
 
 void
-AppServer::MessageReceived(BMessage* message)
+AppServer::RunLooper()
 {
-	switch (message->what) {
+	rename_thread(find_thread(NULL), "picasso");
+	_message_thread((void*)this);
+}
+
+
+/*!	\brief Message handling function for all messages sent to the app_server
+	\param code ID of the message sent
+	\param buffer Attachment buffer for the message.
+
+*/
+void
+AppServer::_DispatchMessage(int32 code, BPrivate::LinkReceiver& msg)
+{
+	switch (code) {
 		case AS_GET_DESKTOP:
 		{
 			Desktop* desktop = NULL;
 
-			int32 userID = message->GetInt32("user", 0);
-			int32 version = message->GetInt32("version", 0);
-			const char* targetScreen = message->GetString("target");
+			port_id replyPort;
+			msg.Read<port_id>(&replyPort);
 
-			if (version != AS_PROTOCOL_VERSION) {
-				syslog(LOG_ERR, "Application for user %" B_PRId32 " does not "
-					"support the current server protocol (%" B_PRId32 ").\n",
-					userID, version);
+			int32 userID;
+			msg.Read<int32>(&userID);
+
+			char* targetScreen = NULL;
+			msg.ReadString(&targetScreen);
+			if (targetScreen != NULL && strlen(targetScreen) == 0) {
+				free(targetScreen);
+				targetScreen = NULL;
+			}
+
+			int32 version;
+			if (msg.Read<int32>(&version) < B_OK
+				|| version != AS_PROTOCOL_VERSION) {
+				fprintf(stderr, "Application for user %" B_PRId32 " with port "
+					"%" B_PRId32 " does not support the current server "
+					"protocol.\n", userID, replyPort);
 			} else {
 				desktop = _FindDesktop(userID, targetScreen);
 				if (desktop == NULL) {
@@ -123,52 +149,56 @@ AppServer::MessageReceived(BMessage* message)
 					// TODO: test if the user exists on the system
 					// TODO: maybe have a separate AS_START_DESKTOP_SESSION for
 					// authorizing the user
-					desktop = _CreateDesktop(userID, targetScreen);
+					desktop = _CreateDesktop(userID, targetScreen ? targetScreen : "SCREEN");
 				}
 			}
 
-			BMessage reply;
-			if (desktop != NULL)
-				reply.AddInt32("port", desktop->MessagePort());
-			else
-				reply.what = (uint32)B_ERROR;
+			free(targetScreen);
 
-			message->SendReply(&reply);
+			BPrivate::LinkSender reply(replyPort);
+			if (desktop != NULL) {
+				reply.StartMessage(B_OK);
+				reply.Attach<port_id>(desktop->MessagePort());
+			} else
+				reply.StartMessage(B_ERROR);
+
+			reply.Flush();
 			break;
 		}
 
-		default:
-			// We don't allow application scripting
-			STRACE(("AppServer received unexpected code %" B_PRId32 "\n",
-				message->what));
-			break;
-	}
-}
-
-
-bool
-AppServer::QuitRequested()
-{
 #if TEST_MODE
-	while (fDesktops.CountItems() > 0) {
-		Desktop *desktop = fDesktops.RemoveItemAt(0);
+		case B_QUIT_REQUESTED:
+		{
+			// We've been asked to quit, so (for now) broadcast to all
+			// desktops to quit. This situation will occur only when the server
+			// is compiled as a regular Be application.
 
-		thread_id thread = desktop->Thread();
-		desktop->PostMessage(B_QUIT_REQUESTED);
+			fQuitting = true;
 
-		// we just wait for the desktop to kill itself
-		status_t status;
-		wait_for_thread(thread, &status);
-	}
+			while (fDesktops.CountItems() > 0) {
+				Desktop *desktop = fDesktops.RemoveItemAt(0);
 
-	delete this;
-	exit(0);
+				thread_id thread = desktop->Thread();
+				desktop->PostMessage(B_QUIT_REQUESTED);
 
-	return SERVER_BASE::QuitRequested();
-#else
-	return false;
+				// we just wait for the desktop to kill itself
+				status_t status;
+				wait_for_thread(thread, &status);
+			}
+
+			delete this;
+
+			// we are now clear to exit
+			exit(0);
+			break;
+		}
 #endif
 
+		default:
+			STRACE(("Server::MainLoop received unexpected code %" B_PRId32 " "
+				"(offset %" B_PRId32 ")\n", code, code - SERVER_TRUE));
+			break;
+	}
 }
 
 
@@ -189,7 +219,7 @@ AppServer::_CreateDesktop(uid_t userID, const char* targetScreen)
 			status = B_NO_MEMORY;
 
 		if (status != B_OK) {
-			syslog(LOG_ERR, "Cannot initialize Desktop object: %s\n",
+			fprintf(stderr, "Cannot initialize Desktop object: %s\n",
 				strerror(status));
 			return NULL;
 		}
@@ -212,10 +242,10 @@ AppServer::_FindDesktop(uid_t userID, const char* targetScreen)
 	for (int32 i = 0; i < fDesktops.CountItems(); i++) {
 		Desktop* desktop = fDesktops.ItemAt(i);
 
-		if (desktop->UserID() == userID
-			&& ((desktop->TargetScreen() == NULL && targetScreen == NULL)
-				|| (desktop->TargetScreen() != NULL && targetScreen != NULL
-					&& strcmp(desktop->TargetScreen(), targetScreen) == 0))) {
+		if (desktop->UserID() == userID) {
+			//&& ((desktop->TargetScreen() == NULL && targetScreen == NULL)
+			//	|| (desktop->TargetScreen() != NULL && targetScreen != NULL
+			//		&& strcmp(desktop->TargetScreen(), targetScreen) == 0))) {
 			return desktop;
 		}
 	}
@@ -230,12 +260,13 @@ AppServer::_FindDesktop(uid_t userID, const char* targetScreen)
 int
 main(int argc, char** argv)
 {
-	srand(real_time_clock_usecs());
+	if (find_port(SERVER_PORT_NAME) >= B_OK)
+		return -1;
 
 	status_t status;
 	AppServer* server = new AppServer(&status);
 	if (status == B_OK)
-		server->Run();
+		server->RunLooper();
 
 	return status == B_OK ? EXIT_SUCCESS : EXIT_FAILURE;
 }
