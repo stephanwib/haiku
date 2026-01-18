61f22852a216a109eed31407749fec1f1d34d473
stephanwib <stephanwib@googlemail.com>
2026-01-18 17:42:42 +0100

    Update Desktop.cpp

--- src/servers/app/Desktop.cpp
+++ src/servers/app/Desktop.cpp
@@ -499,10 +499,11 @@ Desktop::Init()
 	InitializeColorMap();
 
 	//const size_t areaSize = sizeof(server_read_only_memory);
+	const size_t areaSize = 65536;
 	char name[B_OS_NAME_LENGTH];
 	snprintf(name, sizeof(name), "d:%d:shared read only", fUserID);
 	fSharedReadOnlyArea = create_area(name, (void **)&fServerReadOnlyMemory,
-		B_ANY_ADDRESS, 65536, B_NO_LOCK, B_READ_AREA | B_WRITE_AREA | B_CLONEABLE_AREA);
+		B_ANY_ADDRESS, areaSize, B_NO_LOCK, B_READ_AREA | B_WRITE_AREA | B_CLONEABLE_AREA);
 	if (fSharedReadOnlyArea < B_OK) {
 		debug_printf("create_area for fSharedReadOnlyArea failed, size: %d\n", areaSize);
 		return fSharedReadOnlyArea;
