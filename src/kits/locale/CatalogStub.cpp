407ef9e74a511476f61e9ddb432a8d6b85512d32
stephanwib <stephanwib@googlemail.com>
2026-01-23 11:15:09 +0100

    Update CatalogStub.cpp

--- src/kits/locale/CatalogStub.cpp
+++ src/kits/locale/CatalogStub.cpp
@@ -18,11 +18,13 @@ BLocaleRoster::GetCatalog()
 {
 	static BCatalog sCatalog;
 
+	/* Comment out for now to prevent link error
 	#if (__GNUC__ < 3)
 		asm volatile(".hidden GetCatalog__13BLocaleRoster");
 	#else
 		asm volatile(".hidden _ZN13BLocaleRoster10GetCatalogEv");
 	#endif
+	*/
 
 	return _GetCatalog(&sCatalog, &sCatalogInitOnce);
 }
