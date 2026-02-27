2260e1665c0bc77a434f3f8272b1d0873a010dd6
stephanwib <stephanwib@googlemail.com>
2026-02-27 10:47:46 +0100

    Update Model.cpp

--- src/kits/tracker/Model.cpp
+++ src/kits/tracker/Model.cpp
@@ -232,7 +232,7 @@ Model::SetTo(const BEntry* entry, bool open, bool writable)
 	char statbuf[512];
 	status_t err;
 
-	err = entry->GetName(&statbuf);
+	err = entry->GetName(statbuf);
 	if (err == B_OK)
 		printf("getName ok: %s\n", statbuf);
 	else
