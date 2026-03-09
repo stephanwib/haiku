03d4fe68b40369d30e0ae52a24de80d22350dd48
stephanwib <stephanwib@googlemail.com>
2026-03-09 10:25:24 +0100

    Update Directory.cpp

--- src/kits/storage/Directory.cpp
+++ src/kits/storage/Directory.cpp
@@ -566,7 +566,7 @@ BDirectory::CreateSymLink(const char* path, const char* linkToPath,
 	return error;
 }
 
-
+/*
 BDirectory&
 BDirectory::operator=(const BDirectory& dir)
 {
@@ -597,9 +597,9 @@ BDirectory::operator=(const BDirectory& dir)
 	}
 	return *this;
 }
+*/
 
 
-/*
 BDirectory&
 BDirectory::operator=(const BDirectory& dir)
 {
@@ -618,7 +618,7 @@ BDirectory::operator=(const BDirectory& dir)
 	}
 	return *this;
 }
-*/
+
 
 
 
