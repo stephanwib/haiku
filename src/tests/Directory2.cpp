f9383309decfad5414128cddea9f9a196beb2877
stephanwib <stephanwib@googlemail.com>
2026-03-15 18:32:04 +0100

    Create Directory2.cpp

---
+++ src/tests/Directory2.cpp
@@ -0,0 +1,52 @@
+#include <Directory.h>
+#include <Entry.h>
+#include <Path.h>
+#include <stdio.h>
+
+void
+ListDirectory(const char* label, BDirectory& dir)
+{
+    printf("\nListing with %s:\n", label);
+
+    dir.Rewind();
+
+    BEntry entry;
+    while (dir.GetNextEntry(&entry) == B_OK) {
+        char name[B_FILE_NAME_LENGTH];
+        entry.GetName(name);
+        printf("  %s\n", name);
+    }
+}
+
+int
+main(int argc, char** argv)
+{
+    if (argc != 2) {
+        printf("Usage: %s <directory>\n", argv[0]);
+        return 1;
+    }
+
+    const char* path = argv[1];
+
+
+    BDirectory dir1(path);
+    if (dir1.InitCheck() != B_OK) {
+        printf("Failed to open directory from path\n");
+        return 1;
+    }
+
+    printf("dir1 successfully opened\n");
+
+    BDirectory dir2(dir1);
+    if (dir2.InitCheck() != B_OK) {
+        printf("Failed to create dir2 from dir1\n");
+        return 1;
+    }
+
+    printf("dir2 successfully created from dir1\n");
+
+    ListDirectory("dir1", dir1);
+    ListDirectory("dir2", dir2);
+
+    return 0;
+}
