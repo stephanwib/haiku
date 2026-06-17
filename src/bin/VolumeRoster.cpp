054c1aec07a4ddf233610458af6e6e9eb005bcb7
stephanwib <stephanwib@googlemail.com>
2026-06-17 15:57:20 +0200

    Improve VolumeRoster.cpp

--- src/bin/VolumeRoster.cpp
+++ src/bin/VolumeRoster.cpp
@@ -1,5 +1,10 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/sysmacros.h>
 
+#include <Directory.h>
+#include <Entry.h>
+#include <Path.h>
 #include <VolumeRoster.h>
 #include <Volume.h>
 #include <fs_info.h>
@@ -12,25 +17,94 @@ main()
 
 	printf("Listing volumes:\n");
 
-	while (roster.GetNextVolume(&volume) == B_OK) {
+	status_t status;
+	while ((status = roster.GetNextVolume(&volume)) == B_OK) {
 		char name[B_FILE_NAME_LENGTH];
 		fs_info info;
 
-		if (volume.GetName(name) != B_OK)
-			snprintf(name, sizeof(name), "<unnamed>");
-
 		printf("--------------------------------------------------\n");
-		printf("Name: %s\n", name);
-		printf("Device ID: %" B_PRIdDEV "\n", volume.Device());
 
-		if (fs_stat_dev(volume.Device(), &info) == B_OK) {
+		status_t result = volume.GetName(name);
+		if (result != B_OK) {
+			fprintf(stderr, "GetName() failed: %" B_PRId32 "\n", result);
+			snprintf(name, sizeof(name), "<unnamed>");
+		}
+
+		dev_t device = volume.Device();
+
+		printf("[BVolume]\n");
+		printf("Name: %s\n", name);
+		printf("Device ID: %" B_PRIdDEV "\n", device);
+
+		printf("Major: %u\n", (unsigned)major(device));
+		printf("Minor: %u\n", (unsigned)minor(device));
+
+
+		printf("Persistent: %s\n",
+			volume.IsPersistent() ? "yes" : "no");
+		printf("Removable: %s\n",
+			volume.IsRemovable() ? "yes" : "no");
+		printf("Read only: %s\n",
+			volume.IsReadOnly() ? "yes" : "no");
+		printf("Shared: %s\n",
+			volume.IsShared() ? "yes" : "no");
+		printf("Knows MIME: %s\n",
+			volume.KnowsMime() ? "yes" : "no");
+		printf("Knows Attr: %s\n",
+			volume.KnowsAttr() ? "yes" : "no");
+		printf("Knows Query: %s\n",
+			volume.KnowsQuery() ? "yes" : "no");
+
+		printf("\n[Root Directory]\n");
+
+		BDirectory root;
+		result = volume.GetRootDirectory(&root);
+
+		if (result == B_OK) {
+			printf("GetRootDirectory(): B_OK\n");
+
+			BEntry entry;
+			result = root.GetEntry(&entry);
+
+			if (result == B_OK) {
+				printf("GetEntry(): B_OK\n");
+
+				BPath path;
+				result = entry.GetPath(&path);
+
+				if (result == B_OK)
+					printf("Path: %s\n", path.Path());
+				else
+					printf("GetPath(): %" B_PRId32 "\n", result);
+			} else {
+				printf("GetEntry(): %" B_PRId32 "\n", result);
+			}
+		} else {
+			printf("GetRootDirectory(): %" B_PRId32 "\n", result);
+		}
+
+		printf("\n[fs_stat_dev]\n");
+
+		result = fs_stat_dev(device, &info);
+
+		if (result == B_OK) {
 			printf("Filesystem: %s\n", info.fsh_name);
 			printf("Flags: 0x%08" B_PRIx32 "\n", info.flags);
 			printf("Block size: %" B_PRIdOFF "\n", info.block_size);
 			printf("Total blocks: %" B_PRIdOFF "\n", info.total_blocks);
 			printf("Free blocks: %" B_PRIdOFF "\n", info.free_blocks);
+		} else {
+			printf("fs_stat_dev(): %" B_PRId32 "\n", result);
 		}
+
+		printf("\n");
 	}
 
-	return 0;
+	if (status != B_ENTRY_NOT_FOUND) {
+		fprintf(stderr,
+			"GetNextVolume() terminated with error: %" B_PRId32 "\n",
+			status);
+	}
+
+	return status == B_ENTRY_NOT_FOUND ? 0 : 1;
 }
