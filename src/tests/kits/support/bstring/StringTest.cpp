185c34e928b7314c2a207d30b61e8d902fb34d7d
stephanwib <stephanwib@googlemail.com>
2026-06-30 16:25:11 +0200

    Update StringTest.cpp

--- src/tests/kits/support/bstring/StringTest.cpp
+++ src/tests/kits/support/bstring/StringTest.cpp
@@ -25,7 +25,7 @@ CppUnit::Test *StringTestSuite()
 	
 	testSuite->addTest(StringConstructionTest::suite());
 	testSuite->addTest(StringAccessTest::suite());
-	testSuite->addTest(StringAssignTest::suite());
+	// testSuite->addTest(StringAssignTest::suite());
 	testSuite->addTest(StringAppendTest::suite());
 	testSuite->addTest(StringSubCopyTest::suite());
 	testSuite->addTest(StringPrependTest::suite());
