1317be1b8d754516e6bf9e6af4d93bb96d83a4ce
stephanwib <stephanwib@googlemail.com>
2026-07-27 15:08:56 +0200

    Update SDLInterface.cpp

--- src/servers/app/drawing/SDLInterface.cpp
+++ src/servers/app/drawing/SDLInterface.cpp
@@ -46,7 +46,7 @@
 #include "BitmapBuffer.h"
 #include "BBitmapBuffer.h"
 
-// #define DEBUG_SDL_DRIVER
+#define DEBUG_SDL_DRIVER
 
 #ifdef DEBUG_SDL_DRIVER
 #include <stdio.h>
@@ -272,6 +272,7 @@ void SDLEventTranslator(void *arg)
 				/* Keyboard event */
 				case SDL_TEXTINPUT:
 				{
+					STRACE("SDL TEXTINPUT\n");
 					mod = GetModifiers(event);
 
 					if (mod != oldModifiers)
